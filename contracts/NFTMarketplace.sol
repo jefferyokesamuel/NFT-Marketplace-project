9a8a2bd994281b7c4fff6cfeff74f09d5901748f
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2023-04-14 15:54:43 +0100

    Update NFTMarketplace.sol

---
+++ contracts/NFTMarketplace.sol
@@ -0,0 +1,47 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+//Console functions to help debug the smart contract just like in Javascript
+import "hardhat/console.sol";
+//OpenZeppelin's NFT Standard Contracts. We will extend functions from this in our implementation
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract NFTMarketplace is ERC721URIStorage {
+    constructor() ERC721("NFTMarketplace", "NFTM") {
+        owner = payable(msg.sender);
+    }
+
+        using Counters for Counters.Counter;
+    //_tokenIds variable has the most recent minted tokenId
+    Counters.Counter private _tokenIds;
+    //Keeps track of the number of items sold on the marketplace
+    Counters.Counter private _itemsSold;
+    //owner is the contract address that created the smart contract
+    address payable owner;
+    //The fee charged by the marketplace to be allowed to list an NFT
+    uint256 listPrice = 0.01 ether;
+
+    //The structure to store info about a listed token
+    struct ListedToken {
+        uint256 tokenId;
+        address payable owner;
+        address payable seller;
+        uint256 price;
+        bool currentlyListed;
+    }
+
+    //the event emitted when a token is successfully listed
+    event TokenListedSuccess (
+        uint256 indexed tokenId,
+        address owner,
+        address seller,
+        uint256 price,
+        bool currentlyListed
+    );
+
+    //This mapping maps tokenId to token info and is helpful when retrieving details about a tokenId
+    mapping(uint256 => ListedToken) private idToListedToken;
+
+}
