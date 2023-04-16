2df68c474076b089c6072398099f5d35c1c657e2
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2023-04-16 16:21:30 +0100

    changes

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,8 +18,8 @@ module.exports = {
       chainId: 1337
     },
     goerli: {
-      url: process.env.ALCHEMY_API_URL,
-      accounts: [ process.env.ALCHEMY_API_KEY ]
+      url: "https://eth-goerli.g.alchemy.com/v2/bzRA7g_MPnwecp6ECGmBWtpxg3z--1iQ",
+      accounts: [ "4187a03f055041ce5d62f33bf84aed9999ad9344ee86553b99788a1679093d04" ]
     }
   },
   solidity: {
