74330ddfb821019749ea771d622d854c1e614ff2
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2023-04-16 16:23:02 +0100

    Update SellNFT.js

--- src/components/SellNFT.js
+++ src/components/SellNFT.js
@@ -46,6 +46,8 @@ export default function SellNFT () {
     
                 //massage the params to be sent to the create NFT request
                 const price = ethers.utils.parseUnits(formParams.price, 'ether')
+                let listingPrice = await contract.getListPrice()
+                listingPrice = listingPrice.toString()
     
                 //actually create the NFT
                 let transaction = await contract.createToken(metadataURL, price, { value: listingPrice })
