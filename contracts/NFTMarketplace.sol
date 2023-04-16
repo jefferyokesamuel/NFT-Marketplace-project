827bf9a9eff86658322561a691854221fa98153c
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2023-04-16 07:49:29 +0100

    Update NFTMarketplace.sol

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -127,6 +127,7 @@ contract NFTMarketplace is ERC721URIStorage {
         payable(seller).transfer(msg.value);
     }
         listPrice = _listedPrice;
+    function getListPrice() public view returns (uint256) {
     function getLatestIdToListedToken() public view returns (ListedToken memory) {
     function getListedTokenForId(uint256 tokenId) public view returns (ListedToken memory) {
 }
